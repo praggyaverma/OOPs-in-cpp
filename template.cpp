8d530c8e282db122f39cf8d3dc63b8e6e2a97d5f
Praggya <119279862+praggyaverma@users.noreply.github.com>
2024-08-15 01:38:56 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Barbie{
+  public:
+    string name;
+    string movie;
+    int year;
+    
+    Barbie (){
+        cout << "Barbie Object is created!" << endl;
+    }
+
+};
+
+
+int main(){
+
+    return 0;
+}
